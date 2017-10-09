52bdcbf09b3102e3227cc46bb8a89ab3e6418020
shenzhou05 <1092462726@qq.com>
2017-10-09 17:36:08 +0800

    Add NAS Category

---
+++ NASCategory.cs
@@ -0,0 +1,47 @@
+﻿using KSP.UI.Screens;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace AntiSubmarineWeapon
+{
+    public class NASCategoryModule : PartModule
+    {
+
+    }
+
+    [KSPAddon(KSPAddon.Startup.MainMenu, true)]
+    public class NASCategory
+    {
+        private static readonly List<AvailablePart> availableParts = new List<AvailablePart>();
+
+        void Awake()
+        {
+            GameEvents.onGUIEditorToolbarReady.Add(NASCategoryFunc);
+        }
+
+        void NASCategoryFunc()
+        {
+            const string customCategoryName = "NAS";
+            const string customDisplayCategoryName = "NAS Parts";
+            availableParts.Clear();
+            availableParts.AddRange(PartLoader.LoadedPartsList.NASParts());
+            Texture2D iconTex = GameDatabase.Instance.GetTexture("NAS/Plugins/NASIcon", false);
+            RUI.Icons.Selectable.Icon icon = new RUI.Icons.Selectable.Icon("NAS", iconTex, iconTex, false);
+            PartCategorizer.Category filter = PartCategorizer.Instance.filters.Find(f => f.button.categoryName == "Filter by function");
+            PartCategorizer.AddCustomSubcategoryFilter(filter, customCategoryName, customDisplayCategoryName, icon, p => availableParts.Contains(p));
+        }
+    }
+
+    public static class PartLoaderExtensions
+    {
+        public static IEnumerable<AvailablePart> NASParts(this List<AvailablePart> parts)
+        {
+            return (from avPart in parts.Where(p => p.partPrefab)
+                    let NASModule = avPart.partPrefab.GetComponent<NASCategoryModule>()
+                    where NASModule != null
+                    select avPart
+                     ).ToList();
+        }
+    }
+}
